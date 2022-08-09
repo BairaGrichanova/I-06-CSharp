86947bb5f389988aaf58bfb3e2a797ebac8be7d8
Baya Grichanova <bg.red_line@bk.ru>
2022-08-09 16:05:04 +0300

    add Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿//              0   1   2   3   4   5   6   7   8   9
+int[] array = { 18, 72, 63, 54, 45, 18, 27, 17, 15, 65 };
+
+int n = array.Length;
+int find = 18;
+
+int index=0;
+
+while (index < n)
+{
+    if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+    index++;
+}
