7c12a364f8efe77e7bc7e4c6f647c39b55643281
Baya Grichanova <bg.red_line@bk.ru>
2022-08-16 19:52:49 +0300

    add Example 12 - Methods - Array Sort complete

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,4 +1,5 @@
-﻿// Работа с текстом
+﻿/*
+// Работа с текстом
 // Дан текст.
 // Пробелы заменить черточками
 // маленькие "к" заменить на "К"
@@ -31,3 +32,37 @@ string newText = Replace (text, ' ', '|');
 Console.WriteLine (newText);
 
 Console.WriteLine (Replace (newText, 'к', 'К'));
+*/
+
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i=0; i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort (int[] array)
+{
+    for (int i=0; i<array.Length-1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j=i+1; j<array.Length; j++)
+        {
+            if (array[j]<array[minPosition]) minPosition=j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
