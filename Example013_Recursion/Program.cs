ec3709da763411cc427435a3997888ddfc3e37cd
Baya Grichanova <bg.red_line@bk.ru>
2022-08-29 14:52:32 +0300

    I-06 Lectsii - Example 14,15 - add examples

--- Example013_Recursion/Program.cs
+++ Example013_Recursion/Program.cs
@@ -45,7 +45,7 @@ PrintArray (matrix);
 */
 
 // Task 4 Image
-/*
+
 int[,] pic = new int[,]
 {
     { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
@@ -71,35 +71,36 @@ int[,] pic = new int[,]
     { 0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0 },
     { 0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0 },
     { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
-    
+
 };
 void PrintImage(int[,] image)
 {
-for (int i=0; i<image.GetLength(0); i++)
-{
-    for (int j=0; j<image.GetLength(1); j++)
+    for (int i = 0; i < image.GetLength(0); i++)
     {
-        //Console.Write($"{image[i,j]} ");
-        if (image[i,j]==0) Console.Write($"  ");
-        else Console.Write($"00");
-    }
-    Console.WriteLine();
-}}
-void FillImage (int row, int col)
-{
-    if (pic[row,col]==0)
-    {
-        pic[row,col]=1;
-        FillImage(row-1,col);
-        FillImage(row,col-1);
-        FillImage(row+1,col);
-        FillImage(row,col+1);
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            //Console.Write($"{image[i,j]} ");
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
     }
 }
-PrintImage (pic);
-FillImage (13,13);
-PrintImage (pic);
-*/
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+//FillImage (13,13);
+PrintImage(pic);
+
 
 //Task 5 Factorial
 /*
