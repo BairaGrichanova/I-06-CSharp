4c677185114f5338a43c261c12406074c8a9aef4
Baya Grichanova <bg.red_line@bk.ru>
2022-09-06 11:27:14 +0300

    Lectcii - Lesson 7 - add Tasks

---
+++ Example16_/Program.cs
@@ -0,0 +1,178 @@
+﻿// Task 1. Собираем строку
+/*
+string NumberFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = b; i >= a; i--)
+        {
+            result += $"{i} ";
+        }
+    return result;
+}
+
+string NumberRec(int a, int b)
+{
+    if (a < b) return NumberRec(a + 1, b) + $"{a} ";
+    else return $"{a} ";
+}
+Console.WriteLine(NumberFor(1, 10));
+Console.WriteLine(NumberRec(1, 10));
+*/
+// Task 2. Сумма чисел от 1 до n
+/*
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumRec(10)); // 55
+*/
+// Task 3. Факториал числа
+/*
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialFor(10)); // 3628800
+Console.WriteLine(FactorialRec(10)); // 3628800
+*/
+// Task 4. Вычислить a в степени n
+/*
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//    if (n == 0) return 1;
+//    else return PowerRec(a, n - 1) * a;
+}
+int PowerRecMath(int a, int n)
+{
+if (n == 0) return 1;
+else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+else return PowerRecMath(a, n - 1) * a;
+}
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
+Console.WriteLine(PowerRecMath(2, 10)); // 1024
+*/
+// Task 4. Перебор слов
+/*
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("asdf", new char[5]);
+*/
+// Task 4. Обход директории
+/*
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"D:\b.edu4";
+CatalogInfo(path);
+*/
+// Task 5. Пирамидки
+/*
+void Towers(string with = "1", string on = "3", string some = "2", 
+            int count = 5)
+{
+if (count > 1) Towers(with, some, on, count - 1);
+Console.WriteLine($"{with} >> {on}");
+if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers ();
+*/
+// Task 6. Обход разных структур
+/*
+string emp = String.Empty;
+string[] tree = { emp, "=", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// 0 1 2 3 4 5 6 7 8 9 10 11
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.Write(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
+*/
+// Task 7. Фибоначчи
+/*
+decimal fRec = 0;
+decimal fIte = 0;
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+Console.ReadLine();
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte}");
+    fIte = 0;
+}
+Console.WriteLine();
+Console.ReadLine();
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fIte = {fRec}");
+    fRec = 0;
+}
+*/
